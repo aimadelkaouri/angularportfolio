14e3f696ec770e1e9ce8de801e5a7f742ea50534
Aimad Elkaouri <aimadelkaouri@gmail.com>
2025-02-25 01:39:09 +0100

    portfolio

--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+import { Project } from '../_models/Project';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [ProjectCardComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: 'sample Angular App',
+    summary: 'test description',
+    description: '',
+    projectLink: '',
+    tags: ['angular', 'typescript'],
+    pictures: [],
+  }
   constructor(private titleService: Title){
     this.titleService.setTitle('aimad elkaouri - Portfolio');
   }
