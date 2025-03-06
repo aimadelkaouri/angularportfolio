d08df1183c2858e3025d1ffbee9dc3ae050c0724
Aimad Elkaouri <aimadelkaouri@gmail.com>
2025-03-07 00:28:18 +0100

    home service

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, ModalModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- portfolio/src/app/project-card/project-card.component.ts
+++ portfolio/src/app/project-card/project-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../_models/Project';
 import { NgFor } from '@angular/common';
+import { BsModalRef, BsModalService, ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-project-card',
-  imports: [NgFor],
+  imports: [NgFor, ModalModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
